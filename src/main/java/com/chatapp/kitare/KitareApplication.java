93eebbb11ce9c2316aea6cdd07eff1d7261b5f9a
kitare17 <hoctapak@gmail.com>
2023-10-16 21:04:44 +0700

    init

---
+++ src/main/java/com/chatapp/kitare/KitareApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp.kitare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitareApplication.class, args);
+	}
+
+}
